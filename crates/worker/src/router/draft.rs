ff6882628b230192fd885b0e5988311c851dc11e
Jan Schlicht <jan@katulu.io>
2025-09-09 14:07:17 +0200

    chore: cleanup
    remove ununsed crates and files

--- crates/worker/src/router/draft.rs
+++
@@ -1,7 +0,0 @@
-struct Location {
-    path: String,
-}
-trait Router {
-    async fn pull(&self, reference: &Reference) -> Result<Location, Error>;
-    async fn push(&self, reference: &Reference, data: Location) -> Result<(), Error>;
-}
