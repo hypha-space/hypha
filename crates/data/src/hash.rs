5b19796f1740652df5bd049f720bb3e058f248ea
l45k <leonhard@katulu.io>
2025-12-22 11:03:26 +0100

    feat: use rayon and mmap for hashing

--- crates/data/src/hash.rs
+++ crates/data/src/hash.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io, path::Path};
+use std::{io, path::Path};
 
 use blake3::Hasher;
 
@@ -6,10 +6,8 @@ pub fn get_file_hash<P>(path: P) -> io::Result<u64>
 where
     P: AsRef<Path>,
 {
-    let mut file = File::open(path)?;
     let mut hasher = Hasher::new();
-
-    let _n = io::copy(&mut file, &mut hasher)?;
+    hasher.update_mmap_rayon(path)?;
     let hash = hasher.finalize();
 
     let mut buf = [0u8; 8];
