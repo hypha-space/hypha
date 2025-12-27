9f99afee238484c1d96cc977a444edf8057381a4
l45k <leonhard@katulu.io>
2025-12-27 11:19:59 +0100

    refactor: remove time cap in simulation
    we already cap the number of updates and the time heavily depends on the
    task and the workers. To decrease complexity, we can remove this.

--- crates/scheduler/src/scheduling/batch_scheduler.rs
+++ crates/scheduler/src/scheduling/batch_scheduler.rs
@@ -146,8 +146,7 @@ impl TrainingState {
     }
 }
 
-// NOTE: time_cap is u64 (time), update_cap is u32 (count)
-const SIM_TIME_CAP_MS: u64 = 10_000;
+// NOTE: update_cap is u32 (count)
 const SIM_UPDATE_CAP: u32 = 3;
 
 type BatchSizer = Arc<dyn Fn(&Resources) -> u32 + Send + Sync>;
@@ -284,7 +283,6 @@ where
                             &batch_sizes,
                             stats,
                             update_target.saturating_sub(count),
-                            SIM_TIME_CAP_MS,
                             SIM_UPDATE_CAP,
                         );
 
@@ -292,6 +290,7 @@ where
                             time = %time,
                             count = %cnt,
                             peer = %peer_id,
+                            capped,
                             "Simulation with projection {:?} and {:?}",
                             projection,
                             update_target.saturating_sub(count)
@@ -416,7 +415,6 @@ where
                             &batch_sizes,
                             stats,
                             update_target.saturating_sub(count),
-                            SIM_TIME_CAP_MS,
                             SIM_UPDATE_CAP,
                         );
 
@@ -424,6 +422,7 @@ where
                             time = %time,
                             count = %cnt,
                             peer = %peer_id,
+                            capped,
                             "Simulation with projection {:?} and {:?}",
                             projection,
                             update_target.saturating_sub(count)
