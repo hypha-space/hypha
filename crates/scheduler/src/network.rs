7a2d35a82e0b2f392da56b5afd78fecb9c8d7c58
Orlando Hohmeier <orlando@katulu.io>
2026-01-03 17:32:08 +0100

    fix: adjust request timeout
    Change the request timeout calculation so that it scales with the configured RTT beyond 10s but cap
    it at 30s as anything beyond that should probably be handled with retries and longer backoffs.

--- crates/scheduler/src/network.rs
+++ crates/scheduler/src/network.rs
@@ -113,7 +113,7 @@ impl Network {
         let (action_sender, action_receiver) = mpsc::channel(512);
         let meter = metrics::global::meter();
         let request_timeout =
-            (Duration::from_millis(network_config.rtt_ms()) * 10).max(Duration::from_secs(10));
+            (Duration::from_millis(network_config.rtt_ms()) * 100).min(Duration::from_secs(30));
 
         // Build libp2p Swarm using the derived identity and mTLS config
         let swarm = SwarmBuilder::with_existing_identity(cert_chain, private_key, ca_certs, crls)
